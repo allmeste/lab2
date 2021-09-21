2f1f311ec2b11cf5133cdc1b87917a64b41da168
allmeste <90242419+allmeste@users.noreply.github.com>
2021-09-21 20:09:59 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text;
+using System.Linq;
 
 namespace лаба2
 {
@@ -29,16 +30,20 @@ namespace лаба2
             object obj;
 
             //Упаковка и распаковка значимых типов
-            Object o1 = i;
+            int x = 10;
+            Object o1 = x;
             byte m1 = (byte)(int)o1;
 
-            Object o2 = sh;
+            short x1 = 25;
+            Object o2 = x1;
             int m2 = (int)(short)o2;
 
-            Object o3 = sb;
-            char m3 = (char)(sbyte)o3;
+            int x3 = 123;
+            Object o3 = x3;
+            char m3 = (char)(int)o3;
 
             // Работа с неявно типизированной переменной
+            Console.WriteLine("\nРабота с неявно типизированной переменной:");
             var h = 5;
             var a = 12;
             var S = (a * h) / 2;
@@ -52,8 +57,9 @@ namespace лаба2
             Console.WriteLine(z2.HasValue);
             Console.WriteLine(z3);
             //Console.WriteLine(z3.Value);
-            //Объявление и сравнение строковых литералов
 
+            //Объявление и сравнение строковых литералов
+            Console.WriteLine("\nОбъявление и сравнение строковых литералов:");
             Console.WriteLine("Hello world!!!");
             string s1 = "hello";
             string s2 = "world";
@@ -69,22 +75,26 @@ namespace лаба2
 
             // Основные операции
             //Сцепление
+            Console.WriteLine("\nСцепление:");
             string str1 = "hello";
             string str2 = "world";
             string str3 = s1 + " " + s2;
             string str4 = String.Concat(str1, str2);
             Console.WriteLine(str3 + "\n" + str4);
-            //Копирование
+            //
+            Console.WriteLine("\nКопирование:");
             string strA = "Have a good day";
             string strB = string.Copy(strA);
             Console.WriteLine(strB);
-            //Выделение подсторки
+            //
+            Console.WriteLine("\nВыделение подсторки:");
             string s15 = "Привет мир!";
             string s16 = s15.Substring(2);
             string s17 = s15.Substring(4, 4);
             Console.WriteLine(s16);
             Console.WriteLine(s17);
             //Разделение строки на слова
+            Console.WriteLine("\nРазделение строки на слова:");
             string s14 = "Строка для разделения";
             string[] res = s14.Split(' ');
 
@@ -92,7 +102,8 @@ namespace лаба2
             {
                 Console.WriteLine("\n" + s);
             }
-            //ставки подстроки в заданную позицию
+            //вставка подстроки в заданную позицию
+            Console.WriteLine("\nВставка подстроки в заданную позицию:");
             string s11 = "Исходная строка для втавки подстроки";
             string s12 = " |Подстрока, которую вставили|";
             string s13 = s11.Insert(5, s12);
@@ -101,17 +112,16 @@ namespace лаба2
             Console.WriteLine(s13);
 
             //Создайте пустую и null строку. Продемонстрируйте что можно выполнить с такими строками
-
+            Console.WriteLine("\nСоздайте пустую и null строку. Продемонстрируйте что можно выполнить с такими строками");
             string text = "hello";
             string text1 = null;
 
-            Console.WriteLine("Plus: " + (text + text1));
-            Console.WriteLine("Equals: " + (text.Equals(text1)));
-            Console.WriteLine("CompareTo: " + text.CompareTo(text1));
-            Console.WriteLine("String.Concat: " + String.Concat(text, text1));
+            Console.WriteLine("Соединение: " + (text + text1));
+            Console.WriteLine("Сравнение: " + text.CompareTo(text1));
+            Console.WriteLine("Вствка: " + String.Concat(text, text1));
 
             //Создайте строку на основе StringBuilder. Удалите определенные позиции и добавьте новые символы в начало и конец строки.
-
+            Console.WriteLine("\nСоздайте строку на основе StringBuilder. Удалите определенные позиции и добавьте новые символы в начало и конец строки.");
             StringBuilder fit = new StringBuilder("Факультет информационных технологий");
             Console.WriteLine(fit);
             fit.Append("\tБГТУ");
@@ -131,7 +141,7 @@ namespace лаба2
             fit.AppendFormat(" BSTU ");
             Console.WriteLine(fit);
             //Создайте целый двумерный массив и выведите его на консоль в отформатированном виде(матрица). 
-
+            Console.WriteLine("\nСоздайте целый двумерный массив и выведите его на консоль в отформатированном виде(матрица). ");
             int[,] myArr = new int[3, 3];
 
 
@@ -152,7 +162,7 @@ namespace лаба2
                 Console.WriteLine();
             }
             // Создайте одномерный массив строк. Выведите на консоль егосодержимое, длину массива. Поменяйте произвольный элемент(пользователь определяет позицию и значение).
-
+            Console.WriteLine("\n Создайте одномерный массив строк. Выведите на консоль егосодержимое, длину массива. Поменяйте произвольный элемент(пользователь определяет позицию и значение).");
             string[] summerMounths = { "June", "July", "August" };
 
             Console.WriteLine("Длинна массива: " + summerMounths.Length);
@@ -174,6 +184,8 @@ namespace лаба2
                 }
                 Console.WriteLine(summerMounths[i]);
             }
+            Console.WriteLine("\nСоздайте ступечатый(не выровненный) массив вещественных чисел с 3 - мя строками, в каждой из которых 2, 3 и 4 столбцов соответственно.Значения массива введите с консоли.");
+                           
             //            Создайте ступечатый(не выровненный) массив вещественных
             //              чисел с 3 - мя строками, в каждой из которых 2, 3 и 4 столбцов
             //                  соответственно.Значения массива введите с консоли.
@@ -208,24 +220,26 @@ namespace лаба2
             var mass = new[] { 5, 10, 23, 16, 8 };
             var Str = "Hello world!";
 
-            //Задайте кортеж из 5 элементов с типами int, string, char, string,ulong.
+            //Задайте кортеж из 5 элементов с типами int, string, char, string,ulong.Выведите его в консоль полностью и выборочно(1,3,4)
+            Console.WriteLine("\nЗадайте кортеж из 5 элементов с типами int, string, char, string,ulong.Выведите его в консоль полностью и выборочно(1,3,4)");
             (int first, string second, char third, string fourth, ulong fifth) korteg = (10, "hello", 'S', "world", 500);
             Console.WriteLine("Кортеж целиком: "+korteg);
             Console.WriteLine("Выборочные элементы(1, 3, 4): " + korteg.first + " " + korteg.third + " "+ korteg.fourth);
-            // Расспаковка кортежа
 
+            // Расспаковка кортежа
             var (name, age, country) = ("Alla", "18", "Belarus");
             var (a1, b1, c, d1, e) = korteg;
 
             //Cравнениие кортежей
+            Console.WriteLine("\nCравнениие кортежей");
             (int first, string second, char third, string fourth, ulong fifth) korteg1 = (10, "hello", 'S', "world", 500);
             Console.WriteLine( korteg == korteg1);
 
             //Новая функция
-
+            Console.WriteLine("\nСоздайте локальную функцию в main и вызовите ее. ");
             int[] mas = new int[] { 5, 20, 30, 40, 50 };
             string stringg = "Hello";
-            var tuple2 = GetValue(mas, stringg);
+            //var tuple2 = GetValue(mas, stringg);
 
             Console.Write("Массив: ");
 
@@ -235,44 +249,46 @@ namespace лаба2
             }
             Console.WriteLine("\nСтрока:"+stringg);
 
-             (int max, int min, int sum, string letter) GetValue(int[] mas, string st)
+            static (int , int , int , char) GetValue(int[] mas, string st)
             {
-               
-                var resultt = (max: 0, min: 0, sum: 0, letter: "");
-                resultt.min = mas[0];
-                resultt.max = mas[0];
-                resultt.sum = 0;
+                return (mas.Max(), mas.Min(), mas.Sum(), st[0]);
+            }
+            Console.WriteLine("Кортеж из максимального, минимального элемента, их суммы и первого символа строки \n"+GetValue(mas, stringg));
+            //    var resultОfKort = (max: 0, min: 0, sum: 0, letter: "");
+            //    resultОfKort.min = mas[0];
+            //    resultОfKort.max = mas[0];
+            //    resultОfKort.sum = 0;
               
 
-                for (int i = 1; i < mas.Length; i++)
-                {
-                    if (mas[i] > resultt.max)
-                    {
-                        resultt.max = mas[i];
-                    }
-                }
-                for (int i = 1; i < mas.Length; i++)
-                {
-                    if (mas[i] < resultt.min)
-                    {
-                        resultt.min = mas[i];
-                    }
-                }
+            //    for (int i = 1; i < mas.Length; i++)
+            //    {
+            //        if (mas[i] > resultОfKort.max)
+            //        {
+            //            resultОfKort.max = mas[i];
+            //        }
+            //    }
+            //    for (int i = 1; i < mas.Length; i++)
+            //    {
+            //        if (mas[i] < resultОfKort.min)
+            //        {
+            //            resultОfKort.min = mas[i];
+            //        }
+            //    }
 
-                resultt.letter = stringg.Substring(0, 1);
+            //    resultОfKort.letter = st.Substring(0, 1);
 
-                for (int i = 0; i < mas.Length; i++)
-                {
-                    resultt.sum += mas[i];
-                }
+            //    for (int i = 0; i < mas.Length; i++)
+            //    {
+            //        resultОfKort.sum += mas[i];
+            //    }
 
-                return resultt;
-            }
-            Console.WriteLine("Кортеж:"+ tuple2);
-            Console.WriteLine("Max ="+tuple2.max);
-            Console.WriteLine("Min = "+ tuple2.min);
-            Console.WriteLine("Sum = " + tuple2.sum);
-            Console.WriteLine("Letter = "+ tuple2.letter);
+            //    return resultОfKort;
+            //}
+            //Console.WriteLine("Кортеж:"+ tuple2);
+            //Console.WriteLine("Max ="+tuple2.max);
+            //Console.WriteLine("Min = "+ tuple2.min);
+            //Console.WriteLine("Sum = " + tuple2.sum);
+            //Console.WriteLine("Letter = "+ tuple2.letter);
         }
 
     }
